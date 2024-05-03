56e2d71e6af18f40467bca59c9ce473e1d0cd6b9
nishith7 <nishith.upadhya@gmail.com>
2024-05-03 10:39:41 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import {Typography} from '@mui/material'
+
+
+function App() {
+  return (
+    <Typography>
+      Hello
+    </Typography>
+  
+  );
+}
+
+export default App;
